cee65be35e3ec025867e76e23a6bbd953014f0e8
AlizaJawwad1719 <159164016+AlizaJawwad1719@users.noreply.github.com>
2024-10-23 02:03:41 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,21 @@
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
+
+const Homepage = () => {
+  return (
+    <div>
+      
+      <Footer/>
+      <Header/>
+      <Hero/>
+      <Navbar/>
+
+    <h1> This is Homepage. </h1>
+
+    </div>
+  )
+}
+
+export default Homepage
